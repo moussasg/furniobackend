b1f621fbf26de9c80e248b89f5e540de92a134a9
moussasg <117439847+moussasg@users.noreply.github.com>
2023-09-15 16:01:56 +0100

    accept all cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors({
   credentials: true, // Allow credentials
 }));// Set up CORS headers manually
 app.use((req, res, next) => {
-res.header('Access-Control-Allow-Origin', 'https://furnio-frontend-ebkgtobww-moussasg.vercel.app');// url front-end
+res.header('Access-Control-Allow-Origin', '*');// url front-end
 res.header('Access-Control-Allow-Methods', 'GET, POST');
 res.header('Access-Control-Allow-Headers', 'Content-Type');
 res.header('Access-Control-Allow-Credentials', true);
